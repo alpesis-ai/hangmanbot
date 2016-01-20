7637e4c5b45cd680bfec3ba2af02b8a13abeeb12
KellyChan <kwailamchan@hotmail.com>
2016-01-21 02:46:35 +0800

    added multiple split_dicts

--- client/dictionary.py
+++ client/dictionary.py
@@ -2,7 +2,7 @@ import re
 
 import settings
 
-def split_dict(dictpath=settings.DICT_PATH):
+def split_dict(dictpath):
 
     words = {}
 
@@ -15,6 +15,23 @@ def split_dict(dictpath=settings.DICT_PATH):
                 words[wordLength] = [thisLine]
             else:
                 words[wordLength].append(thisLine)
+
+    return words
+
+def split_dicts(dictpaths):
+
+    words = {}
+
+    for dictpath in dictpaths:
+        dictfile = open(dictpath, 'r')
+        for line in dictfile.readlines():
+            thisLine = line.strip()
+            wordLength = len(thisLine)
+            if wordLength > 0:
+                if wordLength not in words.keys():
+                    words[wordLength] = [thisLine]
+                else:
+                    words[wordLength].append(thisLine)
 
     return words
 
@@ -40,8 +57,9 @@ def search_pattern(wordDict, guessWord, pattern):
     matches = []
 
     wordLength = len(guessWord)
-    for word in wordDict[wordLength]:
-        match = re.search(str(pattern), word)
-        matches.append(match.group()) if match else matches
-
+    if wordLength in wordDict.keys():
+        for word in wordDict[wordLength]:
+            match = re.search(str(pattern), word)
+            matches.append(match.group()) if match else matches
+ 
     return matches
